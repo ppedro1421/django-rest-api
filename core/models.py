410bf4374a87e8099735dd4622e9bfd505825a54
ppedro <ppedro.o.verissimo@gmail.com>
2024-04-17 15:15:39 -0300

    CORS update

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Client(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30, blank=False, null=False, unique=True)
-    cnpj = models.CharField(max_length=12, blank=False, null=False, unique=True)
+    cnpj = models.CharField(max_length=30, blank=False, null=False, unique=True)
     created_at = models.DateTimeField(blank=False, null=False, auto_now_add=True)
 
     class Meta:
--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-h+vms!1$=5l^2ri4c7k)8-82k4wjtn*#i&(0u=*2w4y@y#fbf-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
 
 
 # Application definition
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'core'
 ]
 
@@ -49,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'server.urls'
